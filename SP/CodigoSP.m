0d79c0cff05085002f37ca07ef189fd6b250341e
LeoAvMo <112590593+LeoAvMo@users.noreply.github.com>
2023-03-29 10:46:56 -0600

    Update CodigoSP.m

--- SP/CodigoSP.m
+++ SP/CodigoSP.m
@@ -1,3 +1,18 @@
 %% Situacion Problema
 
-disp("Hello World!")
+
+figure
+
+% Graficando las placas infinitas
+xline(0,Color="red")
+x = xline(2,Color="red")
+y = xline(20,Color="blue")
+xline(22,Color="blue")
+hold on
+% Dibujando vectores
+drawArrow = @(x,y) quiver(x(1),y(1),x(2)-x(1),y(2)-y(1),0)
+x1 = [3 19]
+y1 = [19 19]
+drawArrow(x1,y1)
+axis([-30 30 -30 30])          %Límites de la gráfica
+
